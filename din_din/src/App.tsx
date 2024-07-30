7a3a68da082cb2d1bdb7209550cf6b200f260552
Leonardo Guiato <123307371+LeoguiatoM5@users.noreply.github.com>
2024-07-30 20:50:18 -0300

    update_app

--- din_din/src/App.tsx
+++ din_din/src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Signup } from './pages/SignUp';
 
 export function App() {
-  
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Signup />} />
       </Routes>
     </Router>
